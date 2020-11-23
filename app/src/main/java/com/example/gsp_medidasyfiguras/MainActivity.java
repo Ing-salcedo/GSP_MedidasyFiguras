13b9e7aa5a9f9548fcca9ad31d87a9c05af622e9
Gerardo.SalcedoP7 <ingenierosalcedo@outlook.com>
2020-11-22 23:28:46 -0500

    Interfaz inicial bilingue

--- app/src/main/java/com/example/gsp_medidasyfiguras/MainActivity.java
+++ app/src/main/java/com/example/gsp_medidasyfiguras/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.gsp_medidasyfiguras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView lista_opciones;
+    private String opciones[];
+    private ArrayAdapter<String> adapter;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lista_opciones = findViewById(R.id.lstOpciones);
+        opciones = getResources().getStringArray(R.array.opciones);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,opciones);
+        lista_opciones.setAdapter(adapter);
     }
 }
